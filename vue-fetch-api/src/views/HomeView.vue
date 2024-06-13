83a108e2a63da6b54ea9f2a4278432f197a92aa2
unknown <bgsdewoo20@gmail.com>
2024-06-13 20:18:56 +0700

    Menjalankan Logic Lebih Praktis Dengan Watcheffect

--- vue-fetch-api/src/views/HomeView.vue
+++ vue-fetch-api/src/views/HomeView.vue
@@ -3,36 +3,30 @@ import ProductCard from '@/components/ProductCard.vue'
 import Pagination from '@/components/Pagination.vue'
 import Loading from '@/components/Loading.vue'
 
-import { onMounted,ref,watch } from 'vue';
+import { ref, watchEffect} from 'vue';
 import axios from 'axios'
 
 const products = ref([])
 const page = ref(1)
 const limit = ref(8)
-const apiUrl = `http://localhost:3000/products?_page=${page.value}&_per_page=${limit.value}`
 const isLoading = ref(true)
 
-
-onMounted(async () => {
-	try {
-		products.value = await axios.get(apiUrl).then((res) => res.data)
-	} catch(error) {
-		console.log(error)
-	} finally {
-		isLoading.value = false
-	}
-})
-
-
-watch(page, async () => {
+async function fetchData() {
+	const apiUrl = `http://localhost:3000/products?_page=${page.value}&_per_page=${limit.value}`
 	try {
 		isLoading.value = true
-		products.value = await axios.get(apiUrl).then((res) => res.data)
+		const response= await axios.get(apiUrl)
+		products.value = response.data
 	} catch(error) {
-		console.log(error)
+		console.error(error)
 	} finally {
 		isLoading.value = false
 	}
+	
+}
+
+watchEffect(() => {
+	fetchData()
 })
 
 
