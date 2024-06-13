8783d921ba81e4a70152c8ff7986ac62581f48ec
unknown <bgsdewoo20@gmail.com>
2024-06-13 08:59:51 +0700

    Mengenal Component Suspense Dan Cara Kerjanya

--- vue-fetch-api/src/App.vue
+++ vue-fetch-api/src/App.vue
@@ -1,10 +1,20 @@
 <script setup>
 import { RouterLink, RouterView } from 'vue-router'
+import HomeView from './views/HomeView.vue';
 </script>
 
 <template>
   <div>
-    <RouterView />
+    <!-- <RouterView /> -->
+    <Suspense>
+      <template #default>
+        <HomeView />
+      </template>
+      <template #fallback>
+        <p>Loading....</p>
+      </template>
+    </Suspense>
+
 </div>
 </template>
 
